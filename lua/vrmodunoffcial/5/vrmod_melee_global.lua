2e27590be2efc9aa36ef4b48cac18beddac02d1d
Pescorr <102401058+Pescorr@users.noreply.github.com>
2024-07-04 15:58:31 +0900

    2024/07/04

--- lua/vrmodunoffcial/5/vrmod_melee_global.lua
+++ lua/vrmodunoffcial/5/vrmod_melee_global.lua
@@ -31,21 +31,21 @@ end
 local cv_allowgunmelee = CreateConVar("vrmelee_gunmelee", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Allow melee attacks with gun?")
 local cv_allowfist = CreateConVar("vrmelee_fist", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Allow fist attacks?")
 local cv_allowkick = CreateConVar("vrmelee_kick", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Allow kick attacks? (Requires full body tracking)")
-local cv_meleeDamageLow = CreateConVar("vrmelee_damage_low", "1", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Low velocity attack damage")
-local cv_meleeDamageMedium = CreateConVar("vrmelee_damage_medium", "5", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Medium velocity attack damage")
-local cv_meleeDamageHigh = CreateConVar("vrmelee_damage_high", "10", FCVAR_REPLICATED + FCVAR_ARCHIVE, "High velocity attack damage")
-local cv_meleeVelocityLow = CreateConVar("vrmelee_damage_velocity_low", "0.9", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Low velocity attack range")
-local cv_meleeVelocityMedium = CreateConVar("vrmelee_damage_velocity_medium", "2.0", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Medium velocity attack range")
-local cv_meleeVelocityHigh = CreateConVar("vrmelee_damage_velocity_high", "3.9", FCVAR_REPLICATED + FCVAR_ARCHIVE, "High velocity attack range")
-local cv_meleeimpact = CreateConVar("vrmelee_impact", "3", FCVAR_REPLICATED + FCVAR_ARCHIVE)
-local cv_meleeDelay = CreateConVar("vrmelee_delay", "0.01", FCVAR_REPLICATED + FCVAR_ARCHIVE)
+local cv_meleeDamageLow = CreateConVar("vrmelee_damage_low", "1.000", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Low velocity attack damage")
+local cv_meleeDamageMedium = CreateConVar("vrmelee_damage_medium", "5.000", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Medium velocity attack damage")
+local cv_meleeDamageHigh = CreateConVar("vrmelee_damage_high", "10.000", FCVAR_REPLICATED + FCVAR_ARCHIVE, "High velocity attack damage")
+local cv_meleeVelocityLow = CreateConVar("vrmelee_damage_velocity_low", "1.1", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Low velocity attack range")
+local cv_meleeVelocityMedium = CreateConVar("vrmelee_damage_velocity_medium", "2.3", FCVAR_REPLICATED + FCVAR_ARCHIVE, "Medium velocity attack range")
+local cv_meleeVelocityHigh = CreateConVar("vrmelee_damage_velocity_high", "3.3", FCVAR_REPLICATED + FCVAR_ARCHIVE, "High velocity attack range")
+local cv_meleeimpact = CreateConVar("vrmelee_impact", "0.0001", FCVAR_REPLICATED + FCVAR_ARCHIVE)
+local cv_meleeDelay = CreateConVar("vrmelee_delay", "0.0001", FCVAR_REPLICATED + FCVAR_ARCHIVE)
 local cl_usegunmelee = CreateClientConVar("vrmelee_usegunmelee", "1", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Use melee attacks with gun?")
 local cl_usefist = CreateClientConVar("vrmelee_usefist", "1", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Use fist attacks?")
 local cl_usekick = CreateClientConVar("vrmelee_usekick", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Use kick attacks? (Requires full body tracking)")
 local cl_fisteffect = CreateClientConVar("vrmelee_fist_collision", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Use Fist attack Collision?")
 local cl_fistvisible = CreateClientConVar("vrmelee_fist_visible", "0", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Visible Fist Attack Collision Model?")
 local cl_effectmodel = CreateClientConVar("vrmelee_fist_collisionmodel", "models/hunter/plates/plate.mdl", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Fist Attack Collision Model Config")
-local cv_meleeCooldown = CreateClientConVar("vrmelee_cooldown", "0.10", true, FCVAR_REPLICATED + FCVAR_ARCHIVE, "Cooldown time for VR melee attacks")
+local cv_meleeCooldown = CreateClientConVar("vrmelee_cooldown", "0.0001", true, FCVAR_REPLICATED + FCVAR_ARCHIVE, "Cooldown time for VR melee attacks")
 local cv_lefthandcommand = CreateClientConVar("vrmelee_lefthand_command", "", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Command to execute when left hand melee attack hits")
 local cv_righthandcommand = CreateClientConVar("vrmelee_righthand_command", "", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Command to execute when right hand melee attack hits")
 local cv_leftfootcommand = CreateClientConVar("vrmelee_leftfoot_command", "", true, FCVAR_CLIENTCMD_CAN_EXECUTE + FCVAR_ARCHIVE, "Command to execute when left foot melee attack hits")
@@ -55,8 +55,9 @@ local NextMeleeTime = 0
 local cv_emulateblocking = CreateClientConVar("vrmelee_emulateblocking", "0", true, FCVAR_ARCHIVE, "Emulate blocking when hand is rotated 70-100 degrees")
 local cv_emulateblockbutton = CreateClientConVar("vrmelee_emulateblockbutton", "+attack2", true, FCVAR_ARCHIVE, "Button to emulate when blocking")
 local cv_emulateblockrelease = CreateClientConVar("vrmelee_emulateblockbutton_release", "-attack2", true, FCVAR_ARCHIVE, "Button to emulate when blocking")
-local blockingThresholdLow = CreateClientConVar("vrmelee_emulatebloack_Threshold_Low", "70", true, FCVAR_ARCHIVE, "")
-local blockingThresholdHigh = CreateClientConVar("vrmelee_emulatebloack_Threshold_High", "100", true, FCVAR_ARCHIVE, "")
+local blockingThresholdLow = CreateClientConVar("vrmelee_emulatebloack_Threshold_Low", "115", true, FCVAR_ARCHIVE, "")
+local blockingThresholdHigh = CreateClientConVar("vrmelee_emulatebloack_Threshold_High", "180", true, FCVAR_ARCHIVE, "")
+local dummylefthand = CreateClientConVar("vrmod_lefthand", 0, false)
 hook.Add(
     "VRMod_Tracking",
     "vrmod_melee_blocking",
@@ -66,7 +67,7 @@ hook.Add(
         local rightHandAng = g_VR.tracking.pose_righthand.ang
         local leftBlocking = leftHandAng.z >= blockingThresholdLow:GetFloat() and leftHandAng.z <= blockingThresholdHigh:GetFloat()
         local rightBlocking = rightHandAng.z >= blockingThresholdLow:GetFloat() and rightHandAng.z <= blockingThresholdHigh:GetFloat()
-        if leftBlocking or rightBlocking then
+        if (dummylefthand:GetBool() and leftBlocking) or (not dummylefthand:GetBool() and rightBlocking) then
             LocalPlayer():ConCommand(cv_emulateblockbutton:GetString())
         else
             LocalPlayer():ConCommand(cv_emulateblockrelease:GetString())
@@ -85,7 +86,7 @@ if CLIENT then
         function(action, pressed)
             if not IsValid(LocalPlayer()) then return end
             local ply = LocalPlayer()
-            if not ply:Alive() or not IsPlayerInVR(ply) then return end
+            if not ply:Alive() or  ply:InVehicle() or not IsPlayerInVR(ply) then return end
             if NextMeleeTime > CurTime() then return end
             local function GetViewModelTipPosition(vm)
                 local bestDist = 0
@@ -144,13 +145,14 @@ if CLIENT then
                                 )
 
                                 if tr2.Hit then
+                                    NextMeleeTime = CurTime() + cv_meleeCooldown:GetFloat()
                                     net.Start("VRMod_MeleeAttack")
                                     net.WriteFloat(src[1])
                                     net.WriteFloat(src[2])
                                     net.WriteFloat(src[3])
                                     net.WriteVector(vrmod.GetRightHandVelocity():GetNormalized())
                                     net.WriteFloat(meleeDamage)
-                                    net.WriteBool(true) -- gun melee
+                                    net.WriteBool(true)
                                     net.SendToServer()
                                 end
                             end
@@ -191,38 +193,10 @@ if CLIENT then
                     )
 
                     if cl_fisteffect:GetBool() then
-                        local ent = ents.CreateClientProp()
-                        ent:SetModel(cl_effectmodel:GetString())
-                        ent:SetPos(src)
-                        ent:SetAngles(tr2.HitNormal:Angle())
-                        ent:Spawn()
-                        ent:SetSolid(SOLID_VPHYSICS)
-                        ent:SetMoveType(MOVETYPE_VPHYSICS)
-                        ent:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
-                        if cl_fistvisible:GetBool() then
-                            ent:SetRenderMode(RENDERMODE_NORMAL)
-                        else
-                            ent:SetRenderMode(RENDERMODE_ENVIROMENTAL)
-                        end
-
-                        ent:SetNotSolid(true)
-                        local phys = ent:GetPhysicsObject()
-                        if IsValid(phys) then
-                            phys:SetMass(500)
-                            phys:SetDamping(0, 0)
-                            phys:EnableGravity(false)
-                            phys:EnableCollisions(true)
-                            phys:EnableMotion(true)
-                        end
-
-                        table.insert(
-                            meleeBoxes,
-                            {
-                                ent = ent,
-                                time = CurTime()
-                            }
-                        )
-
+                        net.Start("VRMod_SpawnMeleeBox")
+                        net.WriteVector(src)
+                        net.WriteVector(tr2.HitNormal)
+                        net.SendToServer()
                         if cv_lefthandcommand:GetString() ~= "" then
                             LocalPlayer():ConCommand(cv_lefthandcommand:GetString())
                         end
@@ -236,7 +210,7 @@ if CLIENT then
                         net.WriteFloat(src[3])
                         net.WriteVector(vrmod.GetLeftHandVelocity():GetNormalized())
                         net.WriteFloat(meleeDamage)
-                        net.WriteBool(false) -- not gun melee
+                        net.WriteBool(false)
                         net.SendToServer()
                     end
                 end
@@ -274,38 +248,10 @@ if CLIENT then
                     )
 
                     if cl_fisteffect:GetBool() then
-                        local ent = ents.CreateClientProp()
-                        ent:SetModel(cl_effectmodel:GetString())
-                        ent:SetPos(src)
-                        ent:SetAngles(tr2.HitNormal:Angle())
-                        ent:Spawn()
-                        ent:SetSolid(SOLID_BSP)
-                        ent:SetMoveType(MOVETYPE_VPHYSICS)
-                        ent:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
-                        if cl_fistvisible:GetBool() then
-                            ent:SetRenderMode(RENDERMODE_NORMAL)
-                        else
-                            ent:SetRenderMode(RENDERMODE_ENVIROMENTAL)
-                        end
-
-                        ent:SetNotSolid(true)
-                        local phys = ent:GetPhysicsObject()
-                        if IsValid(phys) then
-                            phys:SetMass(100)
-                            phys:SetDamping(0, 0)
-                            phys:EnableGravity(false)
-                            phys:EnableCollisions(true)
-                            phys:EnableMotion(true)
-                        end
-
-                        table.insert(
-                            meleeBoxes,
-                            {
-                                ent = ent,
-                                time = CurTime()
-                            }
-                        )
-
+                        net.Start("VRMod_SpawnMeleeBox")
+                        net.WriteVector(src)
+                        net.WriteVector(tr2.HitNormal)
+                        net.SendToServer()
                         if cv_righthandcommand:GetString() ~= "" then
                             LocalPlayer():ConCommand(cv_righthandcommand:GetString())
                         end
@@ -319,7 +265,7 @@ if CLIENT then
                         net.WriteFloat(src[3])
                         net.WriteVector(vrmod.GetRightHandVelocity():GetNormalized())
                         net.WriteFloat(meleeDamage)
-                        net.WriteBool(false) -- not gun melee
+                        net.WriteBool(false)
                         net.SendToServer()
                     end
                 end
@@ -348,37 +294,10 @@ if CLIENT then
                     )
 
                     if cl_fisteffect:GetBool() then
-                        local ent = ents.CreateClientProp()
-                        ent:SetModel(cl_effectmodel:GetString())
-                        ent:SetPos(src)
-                        ent:SetAngles(tr2.HitNormal:Angle())
-                        ent:Spawn()
-                        ent:SetSolid(SOLID_BSP)
-                        ent:SetMoveType(MOVETYPE_VPHYSICS)
-                        ent:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
-                        if cl_fistvisible:GetBool() then
-                            ent:SetRenderMode(RENDERMODE_NORMAL)
-                        else
-                            ent:SetRenderMode(RENDERMODE_ENVIROMENTAL)
-                        end
-
-                        ent:SetNotSolid(true)
-                        local phys = ent:GetPhysicsObject()
-                        if IsValid(phys) then
-                            phys:SetMass(100)
-                            phys:SetDamping(0, 0)
-                            phys:EnableGravity(false)
-                            phys:EnableCollisions(true)
-                            phys:EnableMotion(true)
-                        end
-
-                        table.insert(
-                            {
-                                ent = ent,
-                                time = CurTime()
-                            }
-                        )
-
+                        net.Start("VRMod_SpawnMeleeBox")
+                        net.WriteVector(src)
+                        net.WriteVector(tr.HitNormal)
+                        net.SendToServer()
                         if cv_leftfootcommand:GetString() ~= "" then
                             LocalPlayer():ConCommand(cv_leftfootcommand:GetString())
                         end
@@ -403,7 +322,7 @@ if CLIENT then
                             net.WriteFloat(src[3])
                             net.WriteVector(g_VR.net[ply:SteamID()].lerpedFrame.leftfootPos:GetNormalized())
                             net.WriteFloat(meleeDamage)
-                            net.WriteBool(false) -- not gun melee  
+                            net.WriteBool(false)
                             net.SendToServer()
                         end
                     end
@@ -433,38 +352,10 @@ if CLIENT then
                     )
 
                     if cl_fisteffect:GetBool() then
-                        local ent = ents.CreateClientProp()
-                        ent:SetModel(cl_effectmodel:GetString())
-                        ent:SetPos(src)
-                        ent:SetAngles(tr2.HitNormal:Angle())
-                        ent:Spawn()
-                        ent:SetSolid(SOLID_BSP)
-                        ent:SetMoveType(MOVETYPE_VPHYSICS)
-                        ent:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
-                        if cl_fistvisible:GetBool() then
-                            ent:SetRenderMode(RENDERMODE_NORMAL)
-                        else
-                            ent:SetRenderMode(RENDERMODE_ENVIROMENTAL)
-                        end
-
-                        ent:SetNotSolid(true)
-                        local phys = ent:GetPhysicsObject()
-                        if IsValid(phys) then
-                            phys:SetMass(100)
-                            phys:SetDamping(0, 0)
-                            phys:EnableGravity(false)
-                            phys:EnableCollisions(true)
-                            phys:EnableMotion(false)
-                        end
-
-                        table.insert(
-                            meleeBoxes,
-                            {
-                                ent = ent,
-                                time = CurTime()
-                            }
-                        )
-
+                        net.Start("VRMod_SpawnMeleeBox")
+                        net.WriteVector(src)
+                        net.WriteVector(tr.HitNormal)
+                        net.SendToServer()
                         if cv_rightfootcommand:GetString() ~= "" then
                             LocalPlayer():ConCommand(cv_rightfootcommand:GetString())
                         end
@@ -489,28 +380,10 @@ if CLIENT then
                             net.WriteFloat(src[3])
                             net.WriteVector(g_VR.net[ply:SteamID()].lerpedFrame.rightfootPos:GetNormalized())
                             net.WriteFloat(meleeDamage)
-                            net.WriteBool(false) -- not gun melee
+                            net.WriteBool(false)
                             net.SendToServer()
                         end
                     end
-                end
-            end
-        end
-    )
-
-    hook.Add(
-        "Think",
-        "VRMeleeBoxes",
-        function()
-            local curTime = CurTime()
-            for i = #meleeBoxes, 1, -1 do
-                local box = meleeBoxes[i]
-                if curTime - box.time > meleeBoxLifetime then
-                    if IsValid(box.ent) then
-                        box.ent:Remove()
-                    end
-
-                    table.remove(meleeBoxes, i)
                 end
             end
         end
@@ -519,6 +392,8 @@ end
 
 if SERVER then
     util.AddNetworkString("VRMod_MeleeAttack")
+    util.AddNetworkString("VRMod_SpawnMeleeBox")
+    local meleeBoxLifetime = 0.1 -- サーバー側でmeleeBoxLifetimeを定義
     net.Receive(
         "VRMod_MeleeAttack",
         function(len, ply)
@@ -545,6 +420,37 @@ if SERVER then
             )
 
             ply:LagCompensation(false)
+        end
+    )
+
+    net.Receive(
+        "VRMod_SpawnMeleeBox",
+        function(len, ply)
+            if not IsValid(ply) or not ply:Alive() then return end
+            local pos = net.ReadVector()
+            local ang = net.ReadVector():Angle()
+            local ent = ents.Create("prop_physics")
+            ent:SetModel(cl_effectmodel:GetString())
+            ent:SetPos(pos)
+            ent:SetAngles(ang)
+            ent:Spawn()
+            ent:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
+            if cl_fistvisible:GetBool() then
+                ent:SetRenderMode(RENDERMODE_NORMAL)
+            else
+                ent:SetRenderMode(RENDERMODE_TRANSALPHA)
+                ent:SetColor(Color(255, 255, 255, 0))
+            end
+
+            local phys = ent:GetPhysicsObject()
+            if IsValid(phys) then
+                phys:SetMass(100)
+                phys:EnableGravity(false)
+                phys:EnableCollisions(true)
+                phys:EnableMotion(false)
+            end
+
+            SafeRemoveEntityDelayed(ent, meleeBoxLifetime) -- 直接値を指定
         end
     )
 end
